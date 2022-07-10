7146a1644325829caab5e984fa492ded4f8be52d
Bui Duc Nhat <nhaths4701@gmail.com>
2022-07-10 23:14:38 +0700

    Update Server

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -2,9 +2,43 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
 
-int main()
+using namespace std;
+
+string SERVER_HOST;
+int SERVER_PORT;
+string USER_DB_PATH, TEAM_DB_PATH, USER_TEAM_DB_PATH;
+
+/**
+* @function handleArguments: set port number for server from arguments user entered when start
+*
+* @param argc: number of argument
+* @param argv: arguments demonstrated in string, each argument is splitted with space.
+*
+* @return 0 if missing or invalid arguments, 1 otherwise
+*/
+int handleArguments(int argc, char** argv) {
+	if (argc != 5) {
+		return 0;
+	}
+	char* stopString;
+	SERVER_HOST = string(argv[1]);
+	SERVER_PORT = strtoul(argv[1], &stopString, 10);
+	USER_DB_PATH = string(argv[2]);
+	TEAM_DB_PATH = string(argv[2]);
+	USER_TEAM_DB_PATH = string(argv[2]);
+	return 1;
+}
+
+int main(int argc, char** argv)
 {
+	if (!handleArguments(argc, argv)) {
+		cout << "Invalid arguments, please try again\n";
+		return 0;
+	}
+
     return 0;
 }
 
