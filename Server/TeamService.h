a11d005223fbf60f7fe12960d0dad12cf67cdb9a
ntkien <ninhtrungkien2811@gmail.com>
2022-07-25 17:11:07 +0700

    Add new prototype

--- Server/TeamService.h
+++ Server/TeamService.h
@@ -11,70 +11,13 @@
 class TeamService {
 private:
 public:
-	TeamService() { }
-	~TeamService() { }
+	TeamService();
+	~TeamService();
 
 	static string DB_PATH;
 
-	static int readDb(vector<Team> &teams) {
-		FILE* fTeam;
-		errno_t error = fopen_s(&fTeam, DB_PATH.c_str(), "rt");
-		if (error != 0) {
-			return 1;
-		}
-
-		char buff[DB_BUFF];
-		vector<Team> tmp;
-		while (fgets(buff, DB_BUFF, fTeam) != NULL) {
-			buff[strlen(buff) - 1] = 0;
-			string name(buff);
-			tmp.push_back(Team(name));
-		}
-		teams = tmp;
-
-		fclose(fTeam);
-		return 0;
-	}
-
-	static bool isExisted(vector<Team> teams, string teamName) {
-		auto teamChecking = std::find_if(teams.begin(), teams.end(), [teamName](Team team) {
-			return team.getName() == teamName;
-		});
-
-		if (teamChecking != teams.end()) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	static string createTeam(vector<UserTeam> &userTeams, vector<Team> &teams, Team team, string username) {
-		FILE* fTeam;
-		errno_t error = fopen_s(&fTeam, DB_PATH.c_str(), "at");
-		if (error != 0) {
-			return RES_UNDEFINED_ERROR;
-		}
-
-		string teamName = team.getName();
-		if (isExisted(teams, teamName)) {
-			fclose(fTeam);
-			return RES_ADDTEAM_EXISTED;
-		} else {
-			if (UserTeamService::createTeam(userTeams, teamName, username)) {
-				fclose(fTeam);
-				return RES_UNDEFINED_ERROR;
-			}
-			teams.push_back(team);
-			string endLine = teamName + "\n";
-
-			fwrite(endLine.c_str(), sizeof(char), endLine.length(), fTeam);
-			fclose(fTeam);
-
-			string path = ROOT_DATA_PATH + teamName;
-			_mkdir(path.c_str());
-
-			return RES_ADDTEAM_SUCCESS;
-		}
-	}
+	static int readDb(vector<Team> &teams);
+	static bool isExisted(vector<Team> teams, string teamName);
+	static string createTeam(vector<UserTeam> &userTeams, vector<Team> &teams, Team team, string username);
 
 };
